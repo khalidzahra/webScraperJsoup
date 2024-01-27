68792f5a415339787080776503e98db709f81942
Saeed-Moradi-1998 <saeed.moradi.edu@gmail.com>
2024-01-27 17:40:52 -0600

    Problem with redundant importers in Chair.java is addressed.

--- src/main/java/org/example/Chair.java
+++ src/main/java/org/example/Chair.java
@@ -1,14 +1,5 @@
 package org.example;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Chair {
     String name;
     String role;
